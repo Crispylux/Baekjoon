f111268504e0a844268462b1437bd72584684ce5
Crispylux <lux020329@gmail.com>
2025-06-30 15:51:10 +0900

    [Bronze V] Title: 합, Time: 168 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args ) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int hap = 0;
+        for(int i = 1; i<=n; i++) {
+            hap += i;
+        }
+        System.out.println(hap);
+        s.close();
+    }
+}
